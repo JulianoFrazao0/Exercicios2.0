83752983479d74158a067384a51db753827ccca2
Juliano Vieira Frazão <65692712+JulianoFrazao0@users.noreply.github.com>
2026-08-25 20:30:23 -0400

    ...

---
+++ src/main/java/org/example/exercicio10.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.Scanner;
+
+public class exercicio10 {
+        public void main (String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Usuário: ");
+            String usuario = sc.next();
+
+            System.out.print("Senha: ");
+            String senha = sc.next();
+
+            if (!usuario.equals("admin")) {
+                System.out.println("Usuário não encontrado.");
+            } else {
+                // Segundo nível (If aninhado): O usuário é admin. A senha está certa?
+                if (senha.equals("java123")) {
+                    System.out.println("Acesso liberado.");
+                } else {
+                    System.out.println("Senha incorreta.");
+                }
+            }
+        }
+}
