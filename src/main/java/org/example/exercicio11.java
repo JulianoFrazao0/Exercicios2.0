a1d0762ee3667410b9b92705a4508293968924ed
Juliano Vieira Frazão <65692712+JulianoFrazao0@users.noreply.github.com>
2026-08-25 20:50:40 -0400

    aticidade 11

--- src/main/java/org/example/exercicio11.java
+++ src/main/java/org/example/exercicio11.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class exercicio11 {
+    public void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a nota do aluno no primeiro Bi: ");
+        double nota1 = sc.nextDouble();
+        System.out.println("Digite a nota do aluno no segundo Bi: ");
+        double nota2 = sc.nextDouble();
+        System.out.println("Digite a nota do aluno no terceiro Bi: ");
+        double nota3 = sc.nextDouble();
+        System.out.println("Digite a nota do aluno no quarto Bi: ");
+        double nota4 = sc.nextDouble();
+
+        System.out.println("Digite a frequncia do aluno: ");
+        double frequencia = sc.nextDouble();
+
+        double calculo = nota1 + nota2 +nota3 + nota4 / 4;
+
+        if (calculo <= 7) {
+            System.out.println("Aprovado");
+
+        } else if (calculo <= 5) {
+            System.out.println("Reprovado");
+//kj
+        }
+    }
 }
