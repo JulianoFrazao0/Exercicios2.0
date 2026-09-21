929eb13698c1e78904079d7cbf7c0e99a766a6c9
Juliano Vieira Frazão <65692712+JulianoFrazao0@users.noreply.github.com>
2026-09-21 14:42:13 -0400

    wasd

--- src/main/java/org/example/exercicio3.java
+++ src/main/java/org/example/exercicio3.java
@@ -6,22 +6,22 @@ public class exercicio3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite a temperatura (em graus Celsius): ");
+        System.out.print("digite a temperatura (em graus Celsius): ");
         double temp = sc.nextDouble();
 
-        System.out.print("Digite a umidade relativa do ar (em %): ");
+        System.out.print("digite a umidade): ");
         double umidade = sc.nextDouble();
 
         if (temp >= 38) {
-            System.out.println("Alerta: muito calor");
+            System.out.println("muito calor");
         }
 
         if (umidade < 30) {
-            System.out.println("Alerta: umidade baixa");
+            System.out.println("umidade baixa");
         }
 
         if (temp >= 35 && umidade < 20) {
-            System.out.println("Alerta: queimada");
+            System.out.println("queimada");
         }
     }
 }
